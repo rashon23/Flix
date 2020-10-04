f93dbb1e421cfd635f0abadb859c11068a9e1488
Rashon <rashonpoole2323@gmail.com>
2020-10-04 16:07:06 -0400

    Added Details Screen and grid view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Rashon Poole on 10/4/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
